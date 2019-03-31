e3295de7df1643c5db79b5235f6196ac58f3be8f
MS10306-16 <MS10306-16>
2019-03-31 19:21:01 +0700

    Initial commit

---
+++ app/src/main/java/com/example/miniproject/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.miniproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView img1,img2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
